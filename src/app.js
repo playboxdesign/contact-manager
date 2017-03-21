3c3c53dcdfcc84efbe275940a8bf29d4b2887496
Lindsey <lindsey@playboxdesign.co.uk>
2017-03-21 15:54:35 +0000

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,23 +1,20 @@
-import {Todo} from './todo';
+import {WebAPI} from './web-api';
+import {HttpClient} from 'aurelia-fetch-client';
 
 export class App {
-  constructor() {
-    this.heading = "Todos";
-    this.todos = [];
-    this.todoDescription = '';
+  static inject() { return [WebAPI]; }
+
+  constructor(api) {
+    this.api = api;
   }
 
-  addTodo() {
-    if (this.todoDescription) {
-      this.todos.push(new Todo(this.todoDescription));
-      this.todoDescription = '';
-    }
-  }
+  configureRouter(config, router) {
+    config.title = 'Contacts';
+    config.map([
+      { route: '',              moduleId: 'no-selection',   title: 'Select'},
+      { route: 'contacts/:id',  moduleId: 'contact-detail', name:'contacts' }
+    ]);
 
-  removeTodo(todo) {
-    let index = this.todos.indexOf(todo);
-    if (index !== -1) {
-      this.todos.splice(index, 1);
-    }
+    this.router = router;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
+import environment from './environment';
+
+//Configure Bluebird Promises.
+Promise.config({
+  warnings: {
+    wForgottenReturn: false
+  }
+});
+
 export function configure(aurelia) {
-  aurelia.use.basicConfiguration();
+  aurelia.use
+    .standardConfiguration()
+    .feature('resources');
+
+  if (environment.debug) {
+    aurelia.use.developmentLogging();
+  }
+
+  if (environment.testing) {
+    aurelia.use.plugin('aurelia-testing');
+  }
+
   aurelia.start().then(() => aurelia.setRoot());
 }
